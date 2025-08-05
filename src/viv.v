d83ae3e9329a134d98e5920cc8a4547d2f43a5d2
⚡ Sigui 🇨🇮 <siguici@proton.me>
2025-08-06 00:00:03 +0100

    🧑‍💻 Instantiate the application at runtime

--- src/viv.v
+++ src/viv.v
@@ -4,24 +4,39 @@ import veb
 import siguici.vite { Vite }
 import siguici.envig { Envig }
 
+@[params]
+pub struct AppConfig {
+pub mut:
+	secret_key string
+	vite       Vite  = Vite.new()
+	config     Envig = Envig.new()
+}
+
 pub struct App {
 	veb.StaticHandler
 pub:
 	secret_key string
 mut:
-	vite   Vite  = Vite.new()
-	config Envig = Envig.new()
+	vite   Vite
+	config Envig
 }
 
-pub fn new_app() &App {
+pub fn App.new() &App {
+	return new_app()
+}
+
+pub fn new_app(config AppConfig) &App {
 	mut app := &App{
-		vite:   Vite.new()
-		config: Envig.new()
+		secret_key: config.secret_key
+		vite:       config.vite
+		config:     config.config
 	}
 	return app
 }
 
-pub fn run[T, U](mut app T, port int) ! {
+pub fn run[T, U](port int) ! {
+	mut app := T{}
+
 	app.handle_static('public', true)!
 
 	veb.run[T, U](mut app, port)
